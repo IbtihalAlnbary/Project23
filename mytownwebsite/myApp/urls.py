e0b147184fdc4145b6a8d9ddab5715f19a62bf9d
IbtihalAlnbary <159442598+IbtihalAlnbary@users.noreply.github.com>
2024-03-30 00:36:03 +0300

    sign up and sign in

--- mytownwebsite/myApp/urls.py
+++ mytownwebsite/myApp/urls.py
@@ -1,1 +1,32 @@
-print("hello")
+
+# from django.contrib import admin
+# from django.urls import path, include
+# from . import views 
+
+# #list of all urls i want add
+# urlpatterns = [
+#     path("", views.home,name="home")
+# ]
+
+
+
+
+from django.urls import path
+from . import views 
+
+#list of all urls i want add
+# urlpatterns = [
+#  path("citizen/",views.index)
+# ]
+
+urlpatterns = [
+   
+ path('',views.home,name="home"),
+ path('signup/',views.signup,name="signup"),
+ path('signin/',views.signin,name="signin"),
+ path('signout/',views.signout,name="signout"),
+ path('homepage/',views.homepage,name="homepage"),
+  path('citizenHomepage/',views.citizenHomePage,name="citizenHomepage")
+
+
+]
